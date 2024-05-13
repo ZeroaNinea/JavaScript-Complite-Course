8faf51b1323b51449218d2bda6928e4474ef7b0e
ZeroaNinea <167541109+ZeroaNinea@users.noreply.github.com>
2024-05-13 15:08:47 +0400

    Add files via upload

---
+++ Forkify again/src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const API_URL_KEY = '87a274b5-7d35-4140-9402-cbdac2321ad0';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const MODAL_CLOSE_SEC = 2.5;
